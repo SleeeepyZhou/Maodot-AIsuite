f4f205936a20eca86ad1a725f1e81fededc1ef53
SleeeepyZhou <sleeeepyzhou@yeah.net>
2024-10-08 23:39:29 +0800

    Test

--- register_types.cpp
+++ register_types.cpp
@@ -4,12 +4,14 @@
 
 #include "core/object/class_db.h"
 #include "ai_object.h"
+#include "stablediffusion.h"
 
 void initialize_ai_suite_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
 	ClassDB::register_class<AIObject>();
+	ClassDB::register_class<StableDiffusion>();
 }
 
 void uninitialize_ai_suite_module(ModuleInitializationLevel p_level) {
